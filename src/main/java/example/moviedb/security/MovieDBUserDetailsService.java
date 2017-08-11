4bc0b2c4228aa2f6f2142505620df4927d6ee8df
detomaso17 <detomasoo@op.pl>
2017-08-11 23:41:41 +0200

    added security (login endpoint)

---
+++ src/main/java/example/moviedb/security/MovieDBUserDetailsService.java
@@ -0,0 +1,42 @@
+package example.moviedb.security;
+
+import example.moviedb.model.User;
+import example.moviedb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MovieDBUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getEnabled(),
+                true,
+                true,
+                true,
+                getAdminAuthority()
+        );
+    }
+
+    private List<GrantedAuthority> getAdminAuthority() {
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("admin"));
+        return authorities;
+    }
+}
