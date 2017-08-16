a4f1067dcfc71f1a9ea81d91d1613ba8f8dd698e
detomaso17 <detomasoo@op.pl>
2017-08-16 10:26:58 +0200

    generating JWT token for sample admin user

--- src/main/java/example/moviedb/config/SecurityConfig.java
+++ src/main/java/example/moviedb/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package example.moviedb.config;
 
+import example.moviedb.security.jwt.JWTAuthenticationFilter;
+import example.moviedb.security.jwt.JWTAuthenticationService;
+import example.moviedb.security.jwt.JWTLoginFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,38 +15,54 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    UserDetailsService movieDbUserDetailsService;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.movieDbUserDetailsService(movieDbUserDetailsService).passwordEncoder(passwordEncoder());
+//    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-            .formLogin()
-                .loginPage("/login")
-                .failureUrl("/login?error")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .and()
-            .logout().logoutSuccessUrl("/login?logout")
-                .and()
-            .csrf()
-                .and()
-            .exceptionHandling().accessDeniedPage("/403");
+                .addFilterBefore(jwtLoginFilter(),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+//                .and()
+////            .formLogin()
+////                .loginPage("/login")
+////                .failureUrl("/login?error")
+////                .usernameParameter("username")
+////                .passwordParameter("password")
+////                .and()
+//                .logout().logoutSuccessUrl("/login?logout")
+//                .and()
+//                .csrf()
+//                .and()
+//                .exceptionHandling().accessDeniedPage("/403")
+
     }
 
     @Bean
@@ -50,5 +70,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         return new BCryptPasswordEncoder();
     }
+
+    public JWTAuthenticationService jwtAuthenticationService() {
+        return new JWTAuthenticationService();
+    }
+
+    public JWTLoginFilter jwtLoginFilter() throws Exception {
+        return new JWTLoginFilter("/login", jwtAuthenticationService(), authenticationManager());
+    }
+
+    public JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter(jwtAuthenticationService());
+    }
+
 
 }
---
+++ src/main/java/example/moviedb/security/jwt/JWTAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package example.moviedb.security.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JWTAuthenticationFilter extends GenericFilterBean {
+
+    private JWTAuthenticationService jwtAuthenticationService;
+
+    public JWTAuthenticationFilter(JWTAuthenticationService jwtAuthenticationService) {
+        this.jwtAuthenticationService = jwtAuthenticationService;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+
+        Authentication authentication = jwtAuthenticationService
+                .getAuthentication((HttpServletRequest) request);
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+}
