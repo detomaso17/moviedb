18947971f3f37b40992f8d96c1b55e98d5875473
detomaso17 <detomasoo@op.pl>
2017-08-19 16:06:19 +0200

    mapping change

--- src/main/java/example/moviedb/controller/HealthcheckController.java
+++ src/main/java/example/moviedb/controller/HealthcheckController.java
@@ -2,6 +2,7 @@ package example.moviedb.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthcheckController {
 
-    @RequestMapping(value = "/status", method = RequestMethod.GET)
+    @GetMapping(value = "/status")
     public ResponseEntity status() {
         return new ResponseEntity<String>(HttpStatus.OK);
     }
