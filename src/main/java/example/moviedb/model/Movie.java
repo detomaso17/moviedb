73cb99df4ab483a03bbdfbbfc6811cff681edba8
detomaso17 <detomasoo@op.pl>
2017-08-21 13:53:08 +0200

    new movie entity

---
+++ src/main/java/example/moviedb/model/Movie.java
@@ -0,0 +1,31 @@
+package example.moviedb.model;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "movies")
+@Data
+@RequiredArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @ManyToOne
+    private User user;
+
+    @NonNull
+    private String title;
+    private String description;
+    @NonNull
+    private Boolean watched;
+}
