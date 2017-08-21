1edba7714f66fe2455a2a9523f3a6c3148bded42
detomaso17 <detomasoo@op.pl>
2017-08-21 13:58:53 +0200

    user repository correction

--- src/main/java/example/moviedb/repository/UserRepository.java
+++ src/main/java/example/moviedb/repository/UserRepository.java
@@ -2,11 +2,8 @@ package example.moviedb.repository;
 
 import example.moviedb.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
 }
