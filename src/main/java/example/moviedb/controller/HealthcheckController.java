6e9f0d7119c64ab45e63d11ae0a8caa9539c8968
detomaso17 <detomasoo@op.pl>
2017-08-22 13:31:07 +0200

    corrected status endpoint

--- src/main/java/example/moviedb/controller/HealthcheckController.java
+++ src/main/java/example/moviedb/controller/HealthcheckController.java
@@ -1,17 +1,14 @@
 package example.moviedb.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import example.moviedb.dto.StatusDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthcheckController {
 
     @GetMapping(value = "/status")
-    public ResponseEntity status() {
-        return new ResponseEntity<String>(HttpStatus.OK);
+    public StatusDto status() {
+        return new StatusDto("ok");
     }
 }
---
+++ src/main/java/example/moviedb/dto/StatusDto.java
@@ -0,0 +1,11 @@
+package example.moviedb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StatusDto {
+
+    private String status;
+}
